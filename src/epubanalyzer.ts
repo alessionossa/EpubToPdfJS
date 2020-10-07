1de7e7964f183e3657763368ac92ec018ccffcac
Alessio Nossa <alessio.nossa@gmail.com>
2020-10-07 21:31:21 +0000

    Added components

---
+++ src/epubanalyzer.ts
@@ -0,0 +1,12 @@
+class EpubAnalyzer {
+
+    directory: string;
+
+    constructor(directory: string) {
+        this.directory = directory;
+    }
+
+    getPages() {
+        
+    }
+}
---
+++ src/pdfgenerator.ts
@@ -0,0 +1,5 @@
+class PdfGenerator {
+    constructor(markup_files) {
+        
+    }
+}
