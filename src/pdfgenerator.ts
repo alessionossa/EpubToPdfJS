319d13d027d4b8837a9a856decfcf0f123897cfd
Alessio Nossa <alessio.nossa@gmail.com>
2020-10-11 12:03:56 +0000

    Working on PDF generation

--- src/pdfgenerator.ts
+++ src/pdfgenerator.ts
@@ -1,6 +1,7 @@
 //import * as ppt from "puppeteer"
 import puppeteer from 'puppeteer'
 import *  as path from "path"
+import *  as fs from "fs"
 
 export class PdfGenerator {
 
@@ -19,21 +20,42 @@ export class PdfGenerator {
         const browser = await puppeteer.launch({
             headless: true,
             args: [
-                '--no-first-run'
+                '--no-first-run',
+                '--no-sandbox',
+                '--allow-file-access-from-files'
             ]
         });
+
+        const pptPage = await browser.newPage();
+
+        //fs.mkdirSync('pdfs');
 
         for (const page of this.pages) {
             console.log("Converting " + page)
 
-            const pptPage = await browser.newPage();
-            const absPagePath = path.resolve(page);
+            
+            let absPagePath = path.resolve(page);
+            absPagePath = absPagePath.replace(/^\//g, 'file:///');
             console.log(absPagePath)
-            pptPage.goto(absPagePath)
+            await pptPage.goto(absPagePath)
 
-            await pptPage.close()
+            const pdfPath = path.join('pdfs', path.basename(page).replace('.xhtml', '.pdf'))
+            await pptPage.pdf({
+                path: pdfPath,
+                height: 980,
+                width: 641,
+                margin: {
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0
+                }
+            })
+            
         }
+        await pptPage.close()
 
+        console.log('---Alle pages converted')
         await browser.close()
     }
 }
